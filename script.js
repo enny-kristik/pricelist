8ebcf2c587c83ef022af6a8d0159a685c623e0d2
marsyadindaaa <adindamarsya33@gmail.com>
2024-11-22 15:17:20 +0700

    yy

---
+++ script.js
@@ -0,0 +1,9 @@
+// script.js
+document.addEventListener("DOMContentLoaded", () => {
+    const menuToggle = document.querySelector(".menu-toggle");
+    const navbarLinks = document.querySelector(".navbar-links");
+
+    menuToggle.addEventListener("click", () => {
+        navbarLinks.classList.toggle("active");
+    });
+});
